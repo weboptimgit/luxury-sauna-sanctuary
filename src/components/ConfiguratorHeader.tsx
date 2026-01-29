391efe3f66e19712281d496af8ffcf6667a8f33d
weboptimgit <gaborik@weboptim.eu>
2026-01-29 19:48:38 +0100

    Update ConfiguratorHeader.tsx

--- src/components/ConfiguratorHeader.tsx
+++ src/components/ConfiguratorHeader.tsx
@@ -15,10 +15,8 @@ const ConfiguratorHeader = () => {
 
   const navItems = [
     { labelKey: 'nav.home', href: `${baseUrl}/`, external: true },
-    { labelKey: 'nav.finnishSaunas', href: `${baseUrl}/k/finske-sauny/`, external: true },
-    { labelKey: 'nav.infraSaunas', href: `${baseUrl}/k/infrasauny/`, external: true },
+    { labelKey: 'nav.finnishSaunas', href: `${baseUrl}/k/premiove-sauny/`, external: true },
     { labelKey: 'nav.hotTubs', href: `${baseUrl}/k/kade/`, external: true },
-    { labelKey: 'nav.accessories', href: `${baseUrl}/k/doplnky/`, external: true },
     { labelKey: 'nav.configurator', href: language === 'en' ? '/en/configurator' : '/konfigurator', external: false },
     { labelKey: 'nav.contact', href: `${baseUrl}/kontakt/`, external: true },
   ];
