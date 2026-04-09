6c4ec38029747927e24890a3ce7a29982adbda5d
weboptim <gaborik@weboptim.eu>
2026-04-09 23:56:38 +0200

    Update Configurator.tsx

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -2075,7 +2075,7 @@ const Configurator = () => {
               >
                 <div className="aspect-[4/3] overflow-hidden">
                   <img
-                    src={saunaCube}
+                    src={modulsaunaGallery1}
                     alt="Sauna"
                     className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                   />
